4200612303fd3779eaade1f908e7a4a451b1be69
吃掉大象的蚂蚁 <eoe2005@qq.com>
2023-11-23 19:10:16 +0800

    微信支付功能开发

--- payment.go
+++ payment.go
@@ -1,15 +1,92 @@
 package g
 
+import (
+	"time"
+)
+
+const (
+	PAYMENT_WX      = "wx"
+	PAYMENT_ALI     = "ali"
+	PAYMENT_YINLIAN = "yinlian"
+)
+
 // 创建订单参数
-type OrderParam struct{}
+type OrderParam struct {
+	PaymentChannel string //支付通道
+	PaymentType    string //支付方式，h5,app,jsapi,native
+	OrderID        string //订单ID
+	OrderTitle     string //订单标题
+	OrderDesc      string //订单描述
+	OrderAttach    string //订单扩展信息
+	OrderAmount    uint64 //订单价格，单位分
+}
+
+// 下单后返回数据
+type OrderOut struct {
+	PaymentChannel string //支付通道
+	PaymentType    string //支付方式，h5,app,jsapi,native
+	OrderID        string //订单ID
+	ResultStr      string //返回的字符串信息
+
+}
 
 // 退款参数
-type RefundParam struct{}
+type RefundParam struct {
+	PaymentChannel string //支付通道
+	OrderID        string //订单ID
+	RefundOrderID  string //退款订单号
+	RefundAmount   int64  //退款金额
+	Msg            string //退款原因
+}
+type RefundOut struct {
+	RefundID string //退款订单号
+}
 
 // 支付相关
 type Payment interface {
-	CreateOrder(OrderParam)                 //创建订单
-	Refund(RefundParam)                     //退款
-	NotifyUrl(*GContext) GHandlerFunc       //支付回掉地址
-	RefundNotifyUrl(*GContext) GHandlerFunc //退款回掉地址
+	CreateOrder(OrderParam) *OrderOut                     //创建订单
+	CloseOrder(OrderParam)                                //关闭订单
+	Refund(RefundParam) *RefundOut                        //退款
+	NotifyUrl(func(*NotifyData) error) GHandlerFunc       //支付回掉地址
+	RefundNotifyUrl(func(*NotifyData) error) GHandlerFunc //退款回掉地址
+}
+type NotifyData struct {
+	PaymentChannel string //支付通道
+	OrderID        string //订单ID
+	RefundOrderID  string //退款订单号
+	IsPayment      bool   //是否是支付
+	IsRefund       bool   //是否是退款
+	IsSuccess      bool   //是否成功
+	PlaformUID     string //第三方平台账号ID
+	TransactionID  string //第三方订单ID
+	Amount         int64  //金额 单位分
+	Ext            string //通知信息
+}
+
+// 微信回掉通知结构
+type WxNotifyBody struct {
+	Mchid               string             `json:"mchid"`
+	TransactionID       string             `json:"transaction_id"`
+	OutTradeNo          string             `json:"out_trade_no"`
+	RefundID            string             `json:"refund_id"`
+	OutRefundNo         string             `json:"out_refund_no"`
+	RefundStatus        string             `json:"refund_status"`
+	TradeState          string             `json:"trade_state"`
+	SuccessTime         time.Time          `json:"success_time"`
+	UserReceivedAccount string             `json:"user_received_account"`
+	Amount              WxNotifyBodyAmount `json:"amount"`
+	Payer               WxNotifyBodyPayer  `json:"payer"`
+	AppID               string             `json:"AppID"`
+	TradeStateDesc      string             `json:"trade_state_desc"`
+	TradeType           string             `json:"trade_type"`
+	Attach              string             `json:"attach"`
+}
+type WxNotifyBodyAmount struct {
+	Total       int64 `json:"total"`
+	Refund      int64 `json:"refund"`
+	PayerTotal  int64 `json:"payer_total"`
+	PayerRefund int64 `json:"payer_refund"`
+}
+type WxNotifyBodyPayer struct {
+	Openid string `json:"openid"`
 }
--- payment_wx.go
+++ payment_wx.go
@@ -1,22 +1,332 @@
 package g
 
-type confPaymentWx struct{}
+import (
+	"context"
+	"encoding/json"
+	"time"
+
+	"github.com/wechatpay-apiv3/wechatpay-go/core"
+	"github.com/wechatpay-apiv3/wechatpay-go/core/auth/verifiers"
+	"github.com/wechatpay-apiv3/wechatpay-go/core/downloader"
+	"github.com/wechatpay-apiv3/wechatpay-go/core/notify"
+	"github.com/wechatpay-apiv3/wechatpay-go/core/option"
+	"github.com/wechatpay-apiv3/wechatpay-go/services/partnerpayments/jsapi"
+	"github.com/wechatpay-apiv3/wechatpay-go/services/partnerpayments/native"
+	"github.com/wechatpay-apiv3/wechatpay-go/services/payments/app"
+	"github.com/wechatpay-apiv3/wechatpay-go/services/payments/h5"
+	"github.com/wechatpay-apiv3/wechatpay-go/services/refunddomestic"
+	"github.com/wechatpay-apiv3/wechatpay-go/utils"
+)
+
+type confPaymentWx struct {
+	AppID           string `yaml:"app_id"`
+	MchID           string `yaml:"mch_id"`
+	MchIdNumber     string `yaml:"mch_id_num"`
+	MchApiV3Key     string `yaml:"mch_api_v3_key"`
+	PriviteKeyPem   string `yaml:"private_key_pem"`
+	NotifyURL       string `yaml:"notify_url"`
+	RefundNotifyURL string `yaml:"refund_notify_url"`
+	client          *core.Client
+	handle          *notify.Handler
+}
 
 // https://github.com/wechatpay-apiv3/wechatpay-go
 // 微信支付回掉
 
 // 创建订单
-func (p *confPaymentWx) CreateOrder(OrderParam) {}
-
-// 退款
-func (p *confPaymentWx) Refund(RefundParam) {}
-
-// 支付回掉地址
-func (p *confPaymentWx) NotifyUrl(*GContext) GHandlerFunc {
+func (p *confPaymentWx) CreateOrder(r OrderParam) *OrderOut {
+	switch r.PaymentType {
+	case "h5":
+		return p.createOrderH5(r)
+	case "app":
+		return p.createOrderApp(r)
+	case "jsapi":
+		return p.createOrderJsApi(r)
+	case "native":
+		return p.createOrderNative(r)
+	}
 	return nil
 }
 
+// 关闭订单
+func (p *confPaymentWx) CloseOrder(r OrderParam) {
+	switch r.PaymentType {
+	case "h5":
+		p.closeOrderH5(r)
+	case "app":
+		p.closeOrderApp(r)
+	case "jsapi":
+		p.closeOrderJsApi(r)
+	case "native":
+		p.closeOrderNative(r)
+	}
+
+}
+
+// https://pay.weixin.qq.com/docs/merchant/apis/refund/refunds/create.html
+// 退款
+func (p *confPaymentWx) Refund(r RefundParam) *RefundOut {
+	svc := refunddomestic.RefundsApiService{Client: p.getClient()}
+	resp, _, err := svc.Create(context.Background(),
+		refunddomestic.CreateRequest{
+			SubMchid:    core.String(p.MchID),
+			OutTradeNo:  core.String(r.OrderID),
+			OutRefundNo: core.String(r.RefundOrderID),
+			Reason:      core.String(r.Msg),
+			NotifyUrl:   core.String(p.RefundNotifyURL),
+			Amount: &refunddomestic.AmountReq{
+				Currency: core.String("CNY"),
+				Refund:   core.Int64(r.RefundAmount),
+				Total:    core.Int64(r.RefundAmount),
+			},
+		},
+	)
+	if err != nil {
+		return nil
+	}
+	return &RefundOut{
+		RefundID: *resp.RefundId,
+	}
+}
+
+// 支付回掉地址
+func (p *confPaymentWx) NotifyUrl(call func(*NotifyData) error) GHandlerFunc {
+	return func(c *GContext) {
+		ret := &WxNotifyBody{}
+		rd, e := p.handle.ParseNotifyRequest(c.Context(), c.Request, ret)
+		if e != nil {
+			return //失败
+		}
+		ext, _ := json.Marshal(rd)
+		args := &NotifyData{
+			PaymentChannel: PAYMENT_WX,
+			TransactionID:  ret.TransactionID,
+			IsPayment:      true,
+			IsRefund:       false,
+			IsSuccess:      ret.TradeState == "SUCCESS",
+			PlaformUID:     ret.Payer.Openid,
+			Amount:         ret.Amount.PayerTotal,
+			RefundOrderID:  "",
+			OrderID:        ret.OutTradeNo,
+			Ext:            string(ext),
+		}
+		if call(args) != nil {
+			return //失败
+		}
+		//成功处理
+	}
+}
+
 // 退款回掉地址
-func (p *confPaymentWx) RefundNotifyUrl(*GContext) GHandlerFunc {
-	return nil
+func (p *confPaymentWx) RefundNotifyUrl(call func(*NotifyData) error) GHandlerFunc {
+	return func(c *GContext) {
+		ret := &WxNotifyBody{}
+		rd, e := p.handle.ParseNotifyRequest(c.Context(), c.Request, ret)
+		if e != nil {
+			return //失败
+		}
+		ext, _ := json.Marshal(rd)
+		args := &NotifyData{
+			PaymentChannel: PAYMENT_WX,
+			TransactionID:  ret.TransactionID,
+			IsPayment:      false,
+			IsRefund:       true,
+			PlaformUID:     ret.Payer.Openid,
+			Amount:         ret.Amount.Refund,
+			RefundOrderID:  ret.OutRefundNo,
+			IsSuccess:      ret.RefundStatus == "SUCCESS",
+			OrderID:        ret.OutTradeNo,
+			Ext:            string(ext),
+		}
+		if call(args) != nil {
+			return //失败
+		}
+		//成功处理
+	}
+}
+
+// h5下单
+func (p *confPaymentWx) createOrderH5(r OrderParam) *OrderOut {
+	svc := h5.H5ApiService{Client: p.getClient()}
+	ctx := context.Background()
+	resp, _, err := svc.Prepay(ctx,
+		h5.PrepayRequest{
+			Appid:         core.String(p.AppID),
+			Mchid:         core.String(p.MchID),
+			Description:   core.String(r.OrderDesc),
+			OutTradeNo:    core.String(r.OrderID),
+			TimeExpire:    core.Time(time.Now().Add(time.Minute * 30)),
+			Attach:        core.String(r.OrderAttach),
+			NotifyUrl:     core.String(p.NotifyURL),
+			SupportFapiao: core.Bool(false),
+			Amount: &h5.Amount{
+				Currency: core.String("CNY"),
+				Total:    core.Int64(int64(r.OrderAmount)),
+			},
+		},
+	)
+	if err != nil {
+		return nil
+	}
+	return &OrderOut{
+		OrderID:        r.OrderID,
+		PaymentChannel: r.PaymentChannel,
+		PaymentType:    r.PaymentType,
+		ResultStr:      *resp.H5Url,
+	}
+}
+
+// App下单
+func (p *confPaymentWx) createOrderApp(r OrderParam) *OrderOut {
+	svc := app.AppApiService{Client: p.getClient()}
+	ctx := context.Background()
+	resp, _, err := svc.Prepay(ctx,
+		app.PrepayRequest{
+			Appid:         core.String(p.AppID),
+			Mchid:         core.String(p.MchID),
+			Description:   core.String(r.OrderDesc),
+			OutTradeNo:    core.String(r.OrderID),
+			TimeExpire:    core.Time(time.Now().Add(time.Minute * 30)),
+			Attach:        core.String(r.OrderAttach),
+			NotifyUrl:     core.String(p.NotifyURL),
+			SupportFapiao: core.Bool(false),
+			Amount: &app.Amount{
+				Currency: core.String("CNY"),
+				Total:    core.Int64(int64(r.OrderAmount)),
+			},
+		},
+	)
+	if err != nil {
+		return nil
+	}
+	return &OrderOut{
+		OrderID:        r.OrderID,
+		PaymentChannel: r.PaymentChannel,
+		PaymentType:    r.PaymentType,
+		ResultStr:      *resp.PrepayId,
+	}
+}
+func (p *confPaymentWx) createOrderJsApi(r OrderParam) *OrderOut {
+	svc := jsapi.JsapiApiService{Client: p.getClient()}
+	ctx := context.Background()
+	resp, _, err := svc.Prepay(ctx,
+		jsapi.PrepayRequest{
+			SpAppid:       core.String(p.AppID),
+			SpMchid:       core.String(p.MchID),
+			Description:   core.String(r.OrderDesc),
+			OutTradeNo:    core.String(r.OrderID),
+			TimeExpire:    core.Time(time.Now().Add(time.Minute * 30)),
+			Attach:        core.String(r.OrderAttach),
+			NotifyUrl:     core.String(p.NotifyURL),
+			SupportFapiao: core.Bool(false),
+			Amount: &jsapi.Amount{
+				Currency: core.String("CNY"),
+				Total:    core.Int64(int64(r.OrderAmount)),
+			},
+		},
+	)
+	if err != nil {
+		return nil
+	}
+	return &OrderOut{
+		OrderID:        r.OrderID,
+		PaymentChannel: r.PaymentChannel,
+		PaymentType:    r.PaymentType,
+		ResultStr:      *resp.PrepayId,
+	}
+}
+func (p *confPaymentWx) createOrderNative(r OrderParam) *OrderOut {
+	svc := native.NativeApiService{Client: p.getClient()}
+	ctx := context.Background()
+	resp, _, err := svc.Prepay(ctx,
+		native.PrepayRequest{
+			SpAppid:       core.String(p.AppID),
+			SpMchid:       core.String(p.MchID),
+			Description:   core.String(r.OrderDesc),
+			OutTradeNo:    core.String(r.OrderID),
+			TimeExpire:    core.Time(time.Now().Add(time.Minute * 30)),
+			Attach:        core.String(r.OrderAttach),
+			NotifyUrl:     core.String(p.NotifyURL),
+			SupportFapiao: core.Bool(false),
+			Amount: &native.Amount{
+				Currency: core.String("CNY"),
+				Total:    core.Int64(int64(r.OrderAmount)),
+			},
+		},
+	)
+	if err != nil {
+		return nil
+	}
+	return &OrderOut{
+		OrderID:        r.OrderID,
+		PaymentChannel: r.PaymentChannel,
+		PaymentType:    r.PaymentType,
+		ResultStr:      *resp.CodeUrl,
+	}
+}
+func (p *confPaymentWx) closeOrderH5(r OrderParam) {
+	svc := h5.H5ApiService{Client: p.getClient()}
+	ctx := context.Background()
+	svc.CloseOrder(ctx,
+		h5.CloseOrderRequest{
+			Mchid:      core.String(p.MchID),
+			OutTradeNo: core.String(r.OrderID),
+		},
+	)
+}
+func (p *confPaymentWx) closeOrderApp(r OrderParam) {
+	svc := app.AppApiService{Client: p.getClient()}
+	ctx := context.Background()
+	svc.CloseOrder(ctx,
+		app.CloseOrderRequest{
+			Mchid:      core.String(p.MchID),
+			OutTradeNo: core.String(r.OrderID),
+		},
+	)
+}
+func (p *confPaymentWx) closeOrderJsApi(r OrderParam) {
+	svc := jsapi.JsapiApiService{Client: p.getClient()}
+	ctx := context.Background()
+	svc.CloseOrder(ctx,
+		jsapi.CloseOrderRequest{
+			SpMchid:    core.String(p.MchID),
+			OutTradeNo: core.String(r.OrderID),
+		},
+	)
+}
+func (p *confPaymentWx) closeOrderNative(r OrderParam) {
+	svc := native.NativeApiService{Client: p.getClient()}
+	ctx := context.Background()
+	svc.CloseOrder(ctx,
+		native.CloseOrderRequest{
+			SpMchid:    core.String(p.MchID),
+			OutTradeNo: core.String(r.OrderID),
+		},
+	)
+}
+
+// 获取连接
+func (p *confPaymentWx) getClient() *core.Client {
+	if p.client == nil {
+		mchPrivateKey, err := utils.LoadPrivateKey(p.PriviteKeyPem)
+		if err != nil {
+			Error("加载微信私钥错误")
+			panic("加载微信私钥错误")
+		}
+		ctx := context.Background()
+		// 使用商户私钥等初始化 client，并使它具有自动定时获取微信支付平台证书的能力
+		opts := []core.ClientOption{
+			option.WithWechatPayAutoAuthCipher(p.MchID, p.MchIdNumber, mchPrivateKey, p.MchApiV3Key),
+		}
+		p.client, err = core.NewClient(ctx, opts...)
+		if err != nil {
+			Error("创建微信支付客户端失败 %s", err.Error())
+			panic("创建微信支付客户端失败 " + err.Error())
+		}
+		downloader.MgrInstance().RegisterDownloaderWithClient(ctx, p.client, p.MchID, p.MchApiV3Key)
+		certificateVisitor := downloader.MgrInstance().GetCertificateVisitor(p.MchID)
+		// 3. 使用证书访问器初始化 `notify.Handler`
+		p.handle = notify.NewNotifyHandler(p.MchApiV3Key, verifiers.NewSHA256WithRSAVerifier(certificateVisitor))
+	}
+	return p.client
 }
